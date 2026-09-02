ef704d1f8992dcf14d467f1903407b186fb12aa4
msperlin <marceloperlin@gmail.com>
2026-09-02 10:12:42 -0300

    Sanitize directory names in slides/AF and regenerate index.html

--- slides/AF/Cap 01 - INTRODUÇÃO A FINANÇAS CORPORATIVAS/script-generate-backgrounds.R
+++
@@ -1,4 +0,0 @@
-bananarama::bananarama(
-    "slides/AF/Cap 01 - INTRODUÇÃO A FINANÇAS CORPORATIVAS/pic-yml/pics.yaml",
-    "backgroundp-pics"
-)
---
+++ slides/AF/Cap-01-INTRODUCAO-A-FINANCAS-CORPORATIVAS/script-generate-backgrounds.R
@@ -0,0 +1,4 @@
+bananarama::bananarama(
+    "slides/AF/Cap-01-INTRODUCAO-A-FINANCAS-CORPORATIVAS/pic-yml/pics.yaml",
+    "backgroundp-pics"
+)
